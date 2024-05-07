e81fcb0d4e6b9f1a1785be0a51fe1707c9b02f8f
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-07 17:16:27 +0530

    import button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Button from '@mui/material/Button';
 import './App.css'
 
 function App() {
@@ -7,7 +7,8 @@ function App() {
 
   return (
     <>
-     
+    <h1>Material UI</h1>
+    <Button variant="contained">Click Me</Button>
     </>
   )
 }
