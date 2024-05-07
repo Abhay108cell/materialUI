3266e03d475e1f04ef84bbf0d416d9612f04416a
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-07 19:37:17 +0530

    edit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './App.css'
 
 function App() {
-  
+  let handleClick=()=>{
+    alert("You clicked the button!");
+  }
 
   return (
     <>
     <h1>Material UI</h1>
-    <Button variant="contained">Click Me</Button>
+    <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon />}>Delete</Button>
+    <Button variant="contained" onClick={handleClick}  color="success"  >Click Me 2</Button>
+    
     </>
   )
 }
