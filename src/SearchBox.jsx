feade80854215eb8f0a39eabd335643271076400
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-08 19:13:47 +0530

    imported textField from materialUI

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,7 +1,12 @@
+import TextField from '@mui/material/TextField';
+
 export default function SearchBox(){
   return(
     <div>
-      Search for the weather
+      <h3>search weather</h3>
+      <form>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
     </div>
   )
 }
